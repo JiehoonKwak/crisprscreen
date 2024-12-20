423ec6e92efe34a77faee7da5abc9fef021eb1e5
Jiehoon Kwak <jiehook@gmail.com>
2024-12-21 01:26:13 +0900

    .

--- scripts/download_geo.py
+++ scripts/download_geo.py
@@ -1,9 +1,23 @@
 import GEOparse
+import os
+njobs = 12
 
 # wang 2022 (Diaz group)
-outdir = "/home/jiehoonk/project/crisprscreen/data/wang2022_NC"
+outdir = "../data/wang2022_NC"
 gse = GEOparse.get_GEO(geo="GSE174554", destdir=outdir, how = 'full')
-gse.download_supplementary_files(directory = outdir, download_sra = False, nproc = 64)
+gse.download_supplementary_files(directory = outdir, download_sra = False, nproc = njobs)
 
-## check metadata
+# park 2024
+## 1. snRNA-seq
+geo="GSE189650"
+outdir = "../data/park2024_PO/RNA"
+os.makedirs(outdir, exist_ok = True)
+gse = GEOparse.get_GEO(geo=geo, destdir=outdir, how = 'full')
+gse.download_supplementary_files(directory = outdir, download_sra = False, nproc = njobs)
 
+## 2. scATAC-seq
+geo="GSE157910"
+outdir = "../data/park2024_PO/ATAC" # sn vs sc?
+os.makedirs(outdir, exist_ok = True)
+gse = GEOparse.get_GEO(geo=geo, destdir=outdir, how = 'full')
+gse.download_supplementary_files(directory = outdir, download_sra = False, nproc = njobs)
