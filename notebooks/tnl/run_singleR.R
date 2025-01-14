6373a546ec996c4bcc863613381bdc4411ab3bf9
JiehoonKwak <jiehoonk@gmail.com>
2025-01-14 12:50:34 +0900

    added singler

---
+++ notebooks/tnl/run_singleR.R
@@ -0,0 +1,36 @@
+library(Seurat)
+library(tidyverse)
+library(here)
+library(SingleR)
+library(celldex)
+
+# TNL
+obj <- readRDS(here('output/tnl2.rds'))
+ref <- HumanPrimaryCellAtlasData()
+counts <- GetAssayData(obj, layer = "counts")
+pred <- SingleR(test = counts, ref = ref, labels = ref$label.main)
+
+obj <- AddMetaData(obj, pred$pruned.labels, col.name = "SingleR")
+
+saveRDS(obj, here("output/tnl2.rds"))
+
+
+# CPTAC
+obj <- readRDS(here('output/cptac2.rds'))
+ref <- HumanPrimaryCellAtlasData()
+counts <- GetAssayData(obj, layer = "counts")
+pred <- SingleR(test = counts, ref = ref, labels = ref$label.main)
+
+obj <- AddMetaData(obj, pred$pruned.labels, col.name = "SingleR")
+
+saveRDS(obj, here("output/cptac2.rds"))
+
+# WANG2024
+obj <- readRDS(here('output/wang2024_2.rds'))
+ref <- HumanPrimaryCellAtlasData()
+counts <- GetAssayData(obj, layer = "counts")
+pred <- SingleR(test = counts, ref = ref, labels = ref$label.main)
+
+obj <- AddMetaData(obj, pred$pruned.labels, col.name = "SingleR")
+
+saveRDS(obj, here("output/wang2024_2.rds"))
