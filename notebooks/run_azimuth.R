bcdf530ee6bfd86228682a85078c1cbe7ffcf89f
JiehoonKwak <jiehoonk@gmail.com>
2025-01-14 14:09:11 +0900

    tnl b azimuth

--- notebooks/run_azimuth.R
+++ notebooks/run_azimuth.R
@@ -1,22 +1,26 @@
 library(Seurat)
 library(Azimuth)
 library(here)
+library(qs)
 source(here('data/gbmap/modified_azimuth.R'))
 ref <- readRDS(here("data/gbmap/azimuth_core_GBmap.rds"))
 
 # TNL - DONE
 obj <- readRDS(here("output/tnl2.rds"))
 obj <- RunAzimuth(obj, reference = ref, annotation.levels = c('annotation_level_3', 'annotation_level_4'))
-saveRDS(obj, here("output/tnl2.rds"))
 
-# CPTAC
+qsave(obj, here("output/tnl2.qs"))
+
+# CPTAC - DONE
 obj <- readRDS(here("output/cptac2.rds"))
 obj <- RunAzimuth(obj, reference = ref, annotation.levels = c('annotation_level_3', 'annotation_level_4'))
 
-saveRDS(obj, here("output/cptac2.rds"))
+qsave(obj, here("output/cptac2.qs"))
 
-# WANG2024
+
+# WANG2024 - DONE
 obj <- readRDS(here("output/wang2024_2.rds"))
 obj <- RunAzimuth(obj, reference = ref, annotation.levels = c('annotation_level_3', 'annotation_level_4'))
 
-saveRDS(obj, here("output/wang2024_2.rds"))
+qsave(obj, here("output/wang2024_2.qs"))
+
